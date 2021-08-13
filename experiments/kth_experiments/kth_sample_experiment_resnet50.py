fixed bug with gradient accumulation and scheduler

experiments/kth_experiments/kth_sample_experiment_resnet50.py
@@ -4,10 +4,10 @@ import time
 import argparse
 import numpy as np
 from multiprocessing import Process
-PROCESS_BATCH_SIZE = 4
-mlflow_exp_name = "\"KTH Only Resnet\""
+PROCESS_BATCH_SIZE = 1
+mlflow_exp_name = "\"KTH Only Resnet50\""
 PYTHON = '/home/gauthiers/.conda/envs/ultra/bin/python'
-RUN_FILE = "parametricSN/cifar_small_sample.py"
+RUN_FILE = "parametricSN/main.py"
 PARAMS_FILE = "parameters_texture.yml"
 OPTIM = "sgd"
 LR = 0.0001
@@ -15,19 +15,19 @@ LRS = 0.1
 LRO = 0.1
 DF = 25
 SEED = int(time.time() * np.random.rand(1))
-EPOCHS = 100
+EPOCHS = 50
 RUNS_PER_SEED = 4
 TOTALRUNS = 1
 SCHEDULER = "OneCycleLR"
 AUGMENT = "original-cifar"
 ACCUM_STEP_MULTIPLE = 128
 TEST_BATCH_SIZE = 128
-TRAIN_BATCH_SIZE = 16
+TRAIN_BATCH_SIZE = 128
 SECOND_ORDER = 0
 MODEL = 'resnet50'
 MODEL_WIDTH = 8
 SCATT_ARCH = 'identity'
-MODEL_LOSS = 'cross-entropy-accum'
+MODEL_LOSS = 'cross-entropy'
 def runCommand(cmd):
     print("[Running] {}".format(cmd))
     os.system(cmd)

experiments/xray_experiments/onlycnn/onlycnn_1000sample_xray_experiment.py
@@ -10,9 +10,9 @@ import numpy as np
 
 from multiprocessing import Process
 
-PROCESS_BATCH_SIZE = 3
+PROCESS_BATCH_SIZE = 1
 
-mlflow_exp_name = "\"04-only cnn 1000 Samples Xray\""
+mlflow_exp_name = "\"Only cnn 1000 Samples Xray\""
 PARAMS_FILE = "parameters_xray.yml"
 PYTHON = '/home/gauthiers/.conda/envs/ultra/bin/python'
 RUN_FILE = "parametricSN/main.py"
@@ -23,16 +23,16 @@ LRO = 0.01
 LRMAX = 0.001
 DF = 25
 SEED = int(time.time() * np.random.rand(1))
-EPOCHS = 300
+EPOCHS = 100
 RUNS_PER_SEED = 10
 TOTALRUNS = 2 * RUNS_PER_SEED
 SCHEDULER = "OneCycleLR"
 TRAIN_SAMPLE_NUM = 1000
-TEST_BATCH_SIZE = 16
-TRAIN_BATCH_SIZE = 16
+TEST_BATCH_SIZE = 4
+TRAIN_BATCH_SIZE = 4
 AUGMENT = "original-cifar"
 SECOND_ORDER = 0
-MODEL = 'resnet50'
+MODEL = 'cnn'
 
 MODEL_WIDTH = 8
 SCATT_ARCH = 'identity'

parametricSN/training/cross_entropy_training_accumulation.py
@@ -56,8 +56,8 @@ def train(model, device, train_loader, scheduler, optimizer, epoch, glicoControl
             model.scatteringBase.saveFilterValues(scatteringActive=True) 
             
             optimizer.zero_grad()
-
-
+            if scheduler != None:
+                scheduler.step()
 
         with torch.no_grad():
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probabilityd