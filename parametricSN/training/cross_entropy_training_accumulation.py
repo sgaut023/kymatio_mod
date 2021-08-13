fix bug gradient

parametricSN/training/cross_entropy_training_accumulation.py
@@ -57,7 +57,10 @@ def train(model, device, train_loader, scheduler, optimizer, epoch, glicoControl
             
             optimizer.zero_grad()
             if scheduler != None:
-                scheduler.step()
+                try:
+                    scheduler.step()
+                except:
+                    pass
 
         with torch.no_grad():
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probabilityd
@@ -69,7 +72,10 @@ def train(model, device, train_loader, scheduler, optimizer, epoch, glicoControl
     model.scatteringBase.saveFilterValues(scatteringActive=True) 
 
     if scheduler != None:
-        scheduler.step()
+        try:
+            scheduler.step()
+        except:
+            pass
 
     
     